5b1f312545414d00d186b1438f855e88c5a4e5b5
Thanga Ganapathy <ganapathy888@gmail.com>
2022-05-14 02:00:12 +0530

    Use enter fn in babel traverse

--- src/astWalk.ts
+++ src/astWalk.ts
@@ -5,32 +5,42 @@ import { Node } from '@babel/types';
 export default function astWalk(ast: Node, code: string, isSource = false) {
   let output = '';
   traverse(ast, {
-    ImportDeclaration(path) {
-      if (path.node.source.value.startsWith('.')) {
-        path.node.specifiers.forEach(() => {
-          const { ast: newAST, code: newCode } = parseJS(
-            path.node.source.value + '.js'
-          );
-          output += astWalk(newAST, newCode);
-        });
-      } else {
+    enter(path) {
+      console.log(path.node.type);
+
+      if (path.isExportDefaultDeclaration()) {
+        if (isSource) {
+          output +=
+            code.slice(path.node.start as number, path.node.end as number) +
+            '\n';
+        } else {
+          const declaration = path.node.declaration;
+          output +=
+            code.slice(declaration.start as number, declaration.end as number) +
+            '\n';
+        }
+
+        path.skip();
+      } else if (path.isImportDeclaration()) {
+        if (path.node.source.value.startsWith('.')) {
+          path.node.specifiers.forEach(() => {
+            const { ast: newAST, code: newCode } = parseJS(
+              path.node.source.value + '.js'
+            );
+            output += astWalk(newAST, newCode);
+          });
+        } else {
+          output +=
+            code.slice(path.node.start as number, path.node.end as number) +
+            '\n';
+        }
+
+        path.skip();
+      } else if (!path.isProgram()) {
         output +=
           code.slice(path.node.start as number, path.node.end as number) + '\n';
+        path.skip();
       }
-    },
-    ExportDefaultDeclaration(path) {
-      if (isSource) {
-        output +=
-          code.slice(path.node.start as number, path.node.end as number) + '\n';
-      } else {
-        const declaration = path.node.declaration;
-        output +=
-          code.slice(declaration.start as number, declaration.end as number) +
-          '\n';
-      }
-    },
-    ExportNamedDeclaration(path) {
-      output += code.slice(path.node.start as number, path.node.end as number);
     },
   });
 
