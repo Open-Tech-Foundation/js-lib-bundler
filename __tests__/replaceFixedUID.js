c5ad7d5af802571fe85c88e3712c124fa2162591
Thanga Ganapathy <ganapathy888@gmail.com>
2022-06-12 10:30:14 +0530

    Fix tests replace fixed uids

--- __tests__/replaceFixedUID.js
+++ __tests__/replaceFixedUID.js
@@ -4,14 +4,20 @@ function replaceFixedUID(str) {
     'e47883ab76bb',
     '21825c70e8a0',
     '6353e21f1574',
+    '301de81736f4',
+    '4f7b8b7dc776',
+    '20710112cfd6',
+    '37bf896c24d2',
+    'c46cf0d7631c',
+    'eb70c824aab9',
   ];
-  const regexp = new RegExp(/_(?:.*)_([0-9a-z]{12})/g);
+  const regexp = new RegExp(/_(?:[^\s]+)_([0-9a-z]{12})/g);
   const matchArr = [...str.matchAll(regexp)];
   const matchedUIDs = matchArr.map((arr) => arr[1]);
   let uidSet;
 
   if (matchedUIDs) {
-    uidSet = new Set(matchedUIDs.slice(1));
+    uidSet = new Set(matchedUIDs);
     let count = 0;
 
     for (let item of uidSet) {
